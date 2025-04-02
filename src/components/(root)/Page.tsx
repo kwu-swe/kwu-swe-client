205cb5c3c8a035ad4a5c49106f1e742fb8c821f4
minhyeong <minhyeong.dev@gmail.com>
2025-04-02 17:26:03 +0900

    feat: create pages

--- src/components/(root)/Page.tsx
+++ src/components/(root)/Page.tsx
@@ -1,8 +1,27 @@
+import Loading from "@/design/Loading";
+import { Button } from "fast-jsx";
 import { cn } from "fast-jsx/util";
+import { useNavigate } from "react-router-dom";
 export default function Root() {
+  const router = useNavigate();
   const container = {
-    displays: "flex justify-center items-center",
+    displays: "flex flex-col gap-y-12 justify-center items-center",
     sizes: "w-full min-h-screen",
   };
-  return <div className={cn(container)}>광운대학교</div>;
+  return (
+    <div className={cn(container)}>
+      <Loading />
+      <Button
+        title="시작하기"
+        onClick={() => router("/sign-in")}
+        option={{
+          background: "bg-kw-brown hover:scale-105 duration-500",
+          textColor: "text-white",
+          font: "text-2xl",
+          width: "w-48",
+          height: "h-12",
+        }}
+      />
+    </div>
+  );
 }
--- src/components/dashboard/Layout.tsx
+++ src/components/dashboard/Layout.tsx
@@ -1,5 +1,22 @@
+import { cn } from "fast-jsx/util";
 import { ReactNode } from "react";
+import Header from "./organism/Header.organism";
+import Navigator from "./organism/Navigator.organism";
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const container = {
+    displays: "flex flex-col",
+  };
+  const body = {
+    displays: "flex",
+  };
+  return (
+    <div className={cn(container)}>
+      <Header />
+      <div className={cn(body)}>
+        <Navigator />
+        {children}
+      </div>
+    </div>
+  );
 }
