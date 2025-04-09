0b41a5567cb14e100806d0d78df8f1305009dfc0
minhyeong <minhyeong.dev@gmail.com>
2025-04-09 15:19:31 +0900

    chore: apply vite config for ```env```

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  resolve: {
-    alias: [
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
-    ],
-  },
-});
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: "@components", replacement: "/src/components" },
+        { find: "@", replacement: "/src" },
+      ],
+    },
+    build: {
+      outDir: "build",
+    },
+    define: {
+      "process.env": process.env,
+    },
+  });
+};
