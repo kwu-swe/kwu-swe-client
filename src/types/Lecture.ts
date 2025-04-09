bab391b37d846c0f972a8f27427e991413d48027
minhyeong <minhyeong.dev@gmail.com>
2025-04-09 15:46:08 +0900

    feat: apply interface of ```Lecture```

--- src/types/Lecture.ts
+++ src/types/Lecture.ts
@@ -5,39 +5,36 @@ interface Course {
   score: string;
 }
 type PeriodType = "MON_0" | "MON_1" | "MON_2" | "MON_3" | "MON_4";
-interface Schedule {
-  id: number;
-  periodType: PeriodType;
-}
 type LectureStatus = "BEFORE" | "IN_PROGRESS" | "COMPLETED";
 type Semester = "FIRST_SEMESTER" | "SECOND_SEMESTER" | "SUMMER" | "WINTER";
 type ProfessorRole = "ROLE_STUDENT" | "ROLE_PROFESSOR" | "ROLE_ADMIN";
+interface Year {
+  value: number;
+  leap: string;
+}
 interface Professor {
   name: string;
   studentNumber: string;
   phoneNumber: string;
   role: ProfessorRole;
 }
+interface CourseResponseDto {
+  courseId: number;
+  courseName: string;
+  courseNumber: string;
+  score: number;
+}
+interface LectureScheduleAndLocation {
+  additionalProp1: string;
+  additionalProp2: string;
+  additionalProp3: string;
+}
 export interface Lecture {
   sizeLimit: number;
-  year: {
-    value: number;
-    leap: boolean;
-  };
+  year: Year;
   lectureStatus: LectureStatus;
   semester: Semester;
   professor: Professor;
-  courseResponseDto: {
-    courseId: number;
-    courseName: string;
-    courseNumber: string;
-    score: number;
-  };
-  lectureScheduleAndLocation: [
-    {
-      additionalProp1: "string";
-      additionalProp2: "string";
-      additionalProp3: "string";
-    }
-  ];
+  courseResponseDto: CourseResponseDto;
+  lectureScheduleAndLocation: LectureScheduleAndLocation[];
 }
