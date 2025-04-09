a80fda43d99322bd407efd3c8d4492716ba5d8a1
minhyeong <minhyeong.dev@gmail.com>
2025-04-09 15:21:34 +0900

    feat: data fetch

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,15 @@
 import Dashboard from "@/components/dashboard/(root)/Page";
+import lectureApi from "@/connection/api/lecture";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export default function Page() {
+  const { data: lectures } = useQuery({
+    queryKey: ["getLectures"],
+    queryFn: lectureApi.get,
+  });
+  useEffect(() => {
+    console.log(lectures);
+  }, [lectures]);
   return <Dashboard />;
 }
