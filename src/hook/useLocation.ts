df9b05402246b6b18f8b35e0a6f1dea09afe43ee
minhyeong <minhyeong.dev@gmail.com>
2025-05-07 21:40:29 +0900

    feat: course admin

--- src/hook/useLocation.ts
+++ src/hook/useLocation.ts
@@ -1,28 +1,25 @@
 import locationApi from "@/connection/api/location";
-import { CreateLocation, UpdateLocation } from "@/types/Location";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { LocationCreate } from "@/types/Location";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function useLocation() {
   const [isCreateMode, setIsCreateMode] = useState<boolean>(false);
-  const { mutate: post, isSuccess: postSuccess } = useMutation({
-    mutationFn: (createLocation: CreateLocation) =>
-      locationApi.post(createLocation),
-    onSuccess: () => setIsCreateMode(false)
+  const queryClient = useQueryClient();
+
+  const { mutate: post, } = useMutation({
+    mutationKey: ['locationPost'],
+    mutationFn: (locationCreate: LocationCreate) =>
+      locationApi.post(locationCreate),
+    onSuccess: () => {
+      setIsCreateMode(false);
+      queryClient.invalidateQueries({ queryKey: ['locationGet'] });
+    }
   });
-
-  // const { mutate: patch, isSuccess: patchSuccess } = useMutation({
-  //   mutationFn: ({ id, data }: { id: number; data: UpdateLocation }) =>
-  //     locationApi.patch(id, data)
-  // });
-
-  // const { mutate: remove, isSuccess: deleteSuccess } = useMutation({
-  //   mutationFn: (id: number) => locationApi.delete(id)
-  // });
-
   const { data, isLoading } = useQuery({
-    queryKey: ["getLocations", postSuccess],
+    queryKey: ['locationGet'],
     queryFn: locationApi.get,
+    staleTime: 0,
   });
 
   return {
