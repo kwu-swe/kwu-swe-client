c8d9a4e91c96ebd694499128fe9e2b8d8358558d
minhyeong <minhyeong.dev@gmail.com>
2025-05-07 21:14:05 +0900

    feat: create ```useSubmission```

---
+++ src/hook/useSubmission.ts
@@ -0,0 +1,58 @@
+import submissionApi from "@/connection/api/submission";
+import { SubmissionCreate } from "@/types/Submission";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+interface Option {
+  studentId: number;
+  assignmentId: number;
+}
+
+export default function useSubmission(option?: Partial<Option>) {
+  const { studentId, assignmentId } = option ?? {};
+
+  const { mutate: post, isSuccess: postSuccess } = useMutation({
+    mutationFn: (data: SubmissionCreate) => submissionApi.post(data)
+  });
+  const { data: studentSubmissions, isLoading: isLoadingStudent } = useQuery({
+    enabled: !!studentId && !assignmentId,
+    queryKey: ["getSubmissionsByStudent", studentId],
+    queryFn: () => submissionApi.getByStudent(studentId!),
+  });
+
+  const { data: assignmentSubmissions, isLoading: isLoadingAssignment } = useQuery({
+    enabled: !!assignmentId && !studentId,
+    queryKey: ["getSubmissionsByAssignment", assignmentId],
+    queryFn: () => submissionApi.getByAssignment(assignmentId!),
+  });
+
+  const { data: submission, isLoading: isLoadingBoth } = useQuery({
+    enabled: !!assignmentId && !!studentId,
+    queryKey: ["getSubmissionByAssignmentAndStudent", assignmentId, studentId],
+    queryFn: () => submissionApi.getByAssignmentAndStudent(assignmentId!, studentId!),
+  });
+  return {
+    submissions: studentSubmissions ?? assignmentSubmissions ?? [],
+    submission,
+    isLoading: isLoadingStudent || isLoadingAssignment || isLoadingBoth,
+    post,
+  };
+}
+
+export function useSubmissionFile(submissionId: number) {
+  const { mutate: upload, isSuccess: uploadSuccess } = useMutation({
+    mutationFn: (file: File) => submissionApi.file.post(submissionId, { file })
+  });
+  const { mutate: remove, isSuccess: deleteSuccess } = useMutation({
+    mutationFn: () => submissionApi.file.delete(submissionId)
+  });
+  const { data: files, isLoading } = useQuery({
+    queryKey: ["getSubmissionFiles", submissionId, uploadSuccess, deleteSuccess],
+    queryFn: () => submissionApi.file.get(submissionId),
+  });
+  return {
+    files: files ?? [],
+    isLoading,
+    upload,
+    delete: remove
+  };
+}
