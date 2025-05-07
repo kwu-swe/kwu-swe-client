be4ac2a88a35008d69cce863f50e3b960596bad4
minhyeong <minhyeong.dev@gmail.com>
2025-05-07 21:13:56 +0900

    feat: create ```useLecture```

--- src/hook/useLecture.ts
+++ src/hook/useLecture.ts
@@ -1,10 +1,25 @@
 import lectureApi from "@/connection/api/lecture";
-import { useQuery } from "@tanstack/react-query";
+import { Lecture, UpdateLecture } from "@/types/Lecture";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export default function useLecture() {
+  const { mutate: post, isSuccess: postSuccess } = useMutation({
+    mutationFn: (lecture: Lecture) => lectureApi.post(lecture)
+  });
+
+  const { mutate: patch, isSuccess: patchSuccess } = useMutation({
+    mutationFn: ({ id, data }: { id: number; data: UpdateLecture }) =>
+      lectureApi.patch(id, data)
+  });
+
+  const { mutate: _delete, isSuccess: deleteSuccess } = useMutation({
+    mutationFn: (id: number) => lectureApi.delete(id)
+  });
+
   const { data, isLoading } = useQuery({
-    queryKey: ["getLectures"],
+    queryKey: ["getLectures", postSuccess, patchSuccess, deleteSuccess],
     queryFn: lectureApi.get,
   });
-  return { lectures: data?.result ?? [], isLoading };
+
+  return { lectures: data ?? [], isLoading, post, patch, delete: _delete };
 }
