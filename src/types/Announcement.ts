95538bdc0e5104058ece60d490c12796c0280e5f
minhyeong <minhyeong.dev@gmail.com>
2025-05-14 22:52:48 +0900

    feat: set interface

--- src/types/Announcement.ts
+++ src/types/Announcement.ts
@@ -1,10 +1,24 @@
 // 공지사항 상태 타입
 
 // 공지사항 인터페이스
-export interface Announcement {
-  id: number;
-  lectureId: number;
-  title: string;
-  content: string;
-  createdAt: Date;
+interface Announcement {
+  announcementId: number,
+  title: string,
+  content: string,
+  writer: string,
+  encodedFiles: string[]
+}
+
+interface AnnouncementSum extends Pick<Announcement, "announcementId" | "title"> {
+  createdAt: Date
+}
+interface AnnouncementCreate extends Omit<Announcement, "announcementId"> { }
+interface AnnouncementUpdate extends Partial<AnnouncementCreate> { }
+
+
+export type {
+  Announcement,
+  AnnouncementSum,
+  AnnouncementCreate,
+  AnnouncementUpdate
 }
