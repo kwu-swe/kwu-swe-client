0dbe9476b65c5b2b7b214b4048c145dd6c685260
minhyeong <minhyeong.dev@gmail.com>
2025-06-05 02:40:36 +0900

    chore: fix command and remove swc
    NOTE: 뭔가 호환이 안되는듯?

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { ConfigEnv, defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv) => {
